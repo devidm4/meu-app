4e4925d52f847d64e62e8ed5203c1c34429554aa
devidm4 <167122230+devidm4@users.noreply.github.com>
2024-05-07 16:51:02 -0300

    first commit

---
+++ meu-site-pessoal-vite/src/App.jsx
@@ -0,0 +1,21 @@
+import {} from 'react'
+import './App.css'
+import devid_avatar from './assets/avatar-devid.jpeg'
+
+function App() {
+  
+  return(
+  <header>
+    <img src={devid_avatar} alt="Foto de Devid"/>
+    <h1>Devid Matheus</h1>
+    <h3>Licenciando Matemática.</h3>
+</header>
+  )
+
+    
+  
+      
+  
+}
+
+export default App
