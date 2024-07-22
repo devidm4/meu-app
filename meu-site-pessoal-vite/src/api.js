f9ca2691e09224fbfafd9bf4c96bd0eb87e2af88
devidm4 <167122230+devidm4@users.noreply.github.com>
2024-07-22 14:36:50 -0300

    biblioteca-dotenv

--- meu-site-pessoal-vite/src/api.js
+++ meu-site-pessoal-vite/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-    baseURL: "https://site-pessoal-api-5r8b.onrender.com/api"  
+    baseURL: url
 });
 
 export default api;
